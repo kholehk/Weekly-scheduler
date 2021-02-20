364ce723617100eef6ab3bd2951fe0970d067295
Oleh L. Khoroshykh <olkhoroshykh@gmail.com>
2021-02-20 22:24:34 +0200

    add style to cell in table

--- src/table/table.js
+++ src/table/table.js
@@ -1,5 +1,6 @@
 import { Template } from '../utils/template';
 import tableHTML from './table.html';
+import './table.scss';
 
 function Table(table, eventKeys) {
   return Template(tableHTML, { table, eventKeys });
